0fef210f4406f1c9095cac387a90105033808e49
AndreyChernukho <andrei.chernukho.86@gmail.com>
2020-10-03 18:37:54 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    let answer = Math.min.apply(null, array)
+    return answer;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    let answer = Math.max.apply(null, array)
+    return answer;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    const sum = array.reduce((a, b) => a + b, 0);
+    return sum/array.length;
 }
